a3278825ee1b29b91c79207b6b02567e471f0735
Nanotoon <nanotooncontact@gmail.com>
2026-03-29 23:42:53 +0800

    update

---
+++ open-next.config.ts
@@ -0,0 +1,18 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({});
+```
+
+Save it as `open-next.config.ts` in `C:\Users\Clyde\Desktop\nanotoon\`
+
+**Important:** When saving in Notepad, change "Save as type" to "All Files" so it doesn't add `.txt` to the end.
+
+Then push again:
+```
+git add .
+```
+```
+git commit -m "add open-next config"
+```
+```
+git push
